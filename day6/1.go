842039b3220f876d93cadebc03c78c71373542a4
m3rashid <m3rashid.hussain@gmail.com>
2025-04-14 03:32:26 +0530

    day 6

--- day6/1.go
+++ day6/1.go
@@ -1,3 +1,70 @@
 package main
 
-func Solution1() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Data struct {
+	time     int
+	distance int
+}
+
+func stringToInt(s string) int {
+	res, _ := strconv.Atoi(s)
+	return res
+}
+
+func parseFile(filename string) []Data {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	timeStrs := strings.Fields(strings.Split(scanner.Text(), ":")[1])
+
+	scanner.Scan()
+	distanceStrs := strings.Fields(strings.Split(scanner.Text(), ":")[1])
+
+	data := make([]Data, len(timeStrs))
+	for i := range len(timeStrs) {
+		data[i] = Data{
+			time:     int(stringToInt(timeStrs[i])),
+			distance: int(stringToInt(distanceStrs[i])),
+		}
+	}
+
+	return data
+}
+
+func process(data Data) int {
+	ret := 0
+	for i := range data.time {
+		speed := i
+		time := data.time - i
+		distance := speed * time
+
+		if distance > data.distance {
+			ret++
+		}
+	}
+
+	return ret
+}
+
+func Solution1() {
+	data := parseFile("input1.txt")
+
+	val := 1
+	for _, d := range data {
+		res := process(d)
+		fmt.Println(d, res)
+		val *= res
+	}
+
+	fmt.Println("Result: ", val)
+}
--- day6/2.go
+++ day6/2.go
@@ -1,3 +1,51 @@
 package main
 
-func Solution2() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func stringToInt64(str string) int64 {
+	res, _ := strconv.ParseInt(str, 10, 64)
+	return res
+}
+
+func parseFile2(filename string) (int64, int64) {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	timeStr := strings.Join(strings.Fields(strings.Split(scanner.Text(), ":")[1]), "")
+
+	scanner.Scan()
+	distanceStr := strings.Join(strings.Fields(strings.Split(scanner.Text(), ":")[1]), "")
+
+	return stringToInt64(timeStr), stringToInt64(distanceStr)
+}
+
+func processFile2(time, distance int64) int64 {
+	var ret int64 = 0
+	for i := range time {
+		s := i
+		t := time - i
+		d := s * t
+
+		if d > distance {
+			ret++
+		}
+	}
+
+	return ret
+
+}
+
+func Solution2() {
+	time, distance := parseFile2("input1.txt")
+	result := processFile2(time, distance)
+	fmt.Println("Result:", result)
+}
