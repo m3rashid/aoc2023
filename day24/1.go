a697369e810a38dda7f783c14a5498e3006141ce
m3rashid <m3rashid.hussain@gmail.com>
2025-04-19 04:02:52 +0530

    day24 part 2 pending

--- day24/1.go
+++ day24/1.go
@@ -2,32 +2,27 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type Point struct {
-	px, py, pz int
-	vx, vy, vz int
+type Hail struct {
+	px, py, pz float64
+	vx, vy, vz float64
 }
 
-func stringToInt(s string) int {
-	// res, _ := strconv.ParseInt(s, 10, 64)
-	// return res
-	res, _ := strconv.Atoi(s)
+func stringToFloat(s string) float64 {
+	res, _ := strconv.ParseFloat(s, 64)
 	return res
 }
 
-func parseFile(filename string) []Point {
+func parseFile(filename string) []Hail {
 	// format: px py pz @ vx vy vz
-	// px, py, pz = position
-	// vx, vy, vz = velocity (distance moved per nanosecond)
 	file, _ := os.Open(filename)
 	defer file.Close()
 
-	allData := make([]Point, 0)
+	allData := make([]Hail, 0)
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
@@ -37,33 +32,69 @@ func parseFile(filename string) []Point {
 		}
 
 		fields := strings.Fields(txt)
-		vals := make([]int, 0)
+		vals := make([]float64, 0)
 		for _, f := range fields {
 			item := strings.Trim(f, ",")
 			if item == "" || item == "@" {
 				continue
 			}
-			vals = append(vals, stringToInt(item))
+			vals = append(vals, stringToFloat(item))
 		}
 
-		allData = append(allData, Point{
-			px: vals[0],
-			py: vals[1],
-			pz: vals[2],
-			vx: vals[3],
-			vy: vals[4],
-			vz: vals[5],
+		allData = append(allData, Hail{
+			vals[0], vals[1], vals[2], // px, py, pz
+			vals[3], vals[4], vals[5], // vx, vy, vz
 		})
 	}
 
 	return allData
 }
 
-func collides(p1, p2 Point) bool {
-	//
+var lowerLimit float64 = 200000000000000
+var upperLimit float64 = 400000000000000
+
+func collides(a, b Hail) bool {
+	xa := a.vx
+	xb := -1 * b.vx
+	xc := a.vy
+	xd := -1 * b.vy
+	xe := b.px - a.px
+	xf := b.py - a.py
+
+	den := xa*xd - xb*xc
+	if den == 0 {
+		return false
+	}
+
+	t1 := (xe*xd - xf*xb) / den
+	t2 := (xa*xf - xe*xc) / den
+
+	if t1 < 0 || t2 < 0 {
+		return false
+	}
+
+	x := a.px + t1*a.vx
+	y := a.py + t1*a.vy
+
+	if x < lowerLimit || x > upperLimit || y < lowerLimit || y > upperLimit {
+		return false
+	}
+
+	return true
 }
 
 func Solution1() {
-	allPoints := parseFile("sample.txt")
-	fmt.Println("allData: ", allPoints)
+	allPoints := parseFile("input1.txt")
+	length := len(allPoints)
+
+	numCollisions := 0
+
+	for i := range length {
+		for j := i + 1; j < length; j++ {
+			if collides(allPoints[i], allPoints[j]) {
+				numCollisions++
+			}
+		}
+	}
+	println("Number of collisions: ", numCollisions)
 }
--- day24/2.go
+++ day24/2.go
@@ -1,3 +1,110 @@
 package main
 
-func Solution2() {}
+import "fmt"
+
+func determinant2x2(mat [2][2]float64) float64 {
+	return mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0]
+}
+
+func determinant3x3(mat [3][3]float64) float64 {
+	return mat[0][0]*(mat[1][1]*mat[2][2]-mat[1][2]*mat[2][1]) -
+		mat[0][1]*(mat[1][0]*mat[2][2]-mat[1][2]*mat[2][0]) +
+		mat[0][2]*(mat[1][0]*mat[2][1]-mat[1][1]*mat[2][0])
+}
+
+func determinant4x4(mat [4][4]float64) float64 {
+	return mat[0][0]*determinant3x3([3][3]float64{
+		{mat[1][1], mat[1][2], mat[1][3]},
+		{mat[2][1], mat[2][2], mat[2][3]},
+		{mat[3][1], mat[3][2], mat[3][3]},
+	}) -
+		mat[0][1]*determinant3x3([3][3]float64{
+			{mat[1][0], mat[1][2], mat[1][3]},
+			{mat[2][0], mat[2][2], mat[2][3]},
+			{mat[3][0], mat[3][2], mat[3][3]},
+		}) +
+		mat[0][2]*determinant3x3([3][3]float64{
+			{mat[1][0], mat[1][1], mat[1][3]},
+			{mat[2][0], mat[2][1], mat[2][3]},
+			{mat[3][0], mat[3][1], mat[3][3]},
+		}) -
+		mat[0][3]*determinant3x3([3][3]float64{
+			{mat[1][0], mat[1][1], mat[1][2]},
+			{mat[2][0], mat[2][1], mat[2][2]},
+			{mat[3][0], mat[3][1], mat[3][2]},
+		})
+}
+
+func result(a, b, c, d, e Hail) (float64, float64, float64) {
+	D := determinant4x4([4][4]float64{
+		{a.vx - b.vx, b.vy - a.vy, a.py - b.py, b.px - a.px},
+		{b.vx - c.vx, c.vy - b.vy, b.py - c.py, c.px - b.px},
+		{c.vx - d.vx, d.vy - c.vy, c.py - d.py, d.px - c.px},
+		{d.vx - e.vx, e.vy - d.vy, d.py - e.py, e.px - d.px},
+	})
+
+	resMat := [4]float64{
+		b.px*b.vy - b.py*b.vx + a.py*a.vx - a.px*a.vy,
+		c.px*c.vy - c.py*c.vx + b.py*b.vx - b.px*b.vy,
+		d.px*d.vy - d.py*d.vx + c.py*c.vx - c.px*c.vy,
+		e.px*e.vy - e.py*e.vx + d.py*d.vx - d.px*d.vy,
+	}
+
+	DY := determinant4x4([4][4]float64{
+		{resMat[0], b.vy - a.vy, a.py - b.py, b.px - a.px},
+		{resMat[1], c.vy - b.vy, b.py - c.py, c.px - b.px},
+		{resMat[2], d.vy - c.vy, c.py - d.py, d.px - c.px},
+		{resMat[3], e.vy - d.vy, d.py - e.py, e.px - d.px},
+	})
+
+	DX := determinant4x4([4][4]float64{
+		{a.vx - b.vx, resMat[0], a.py - b.py, b.px - a.px},
+		{b.vx - c.vx, resMat[1], b.py - c.py, c.px - b.px},
+		{c.vx - d.vx, resMat[2], c.py - d.py, d.px - c.px},
+		{d.vx - e.vx, resMat[3], d.py - e.py, e.px - d.px},
+	})
+
+	DVX := determinant4x4([4][4]float64{
+		{a.vx - b.vx, b.vy - a.vy, resMat[0], b.px - a.px},
+		{b.vx - c.vx, c.vy - b.vy, resMat[1], c.px - b.px},
+		{c.vx - d.vx, d.vy - c.vy, resMat[2], d.px - c.px},
+		{d.vx - e.vx, e.vy - d.vy, resMat[3], e.px - d.px},
+	})
+
+	// DVY := determinant4x4([4][4]float64{
+	// 	{a.vx - b.vx, b.vy - a.vy, a.py - b.py, resMat[0]},
+	// 	{b.vx - c.vx, c.vy - b.vy, b.py - c.py, resMat[1]},
+	// 	{c.vx - d.vx, d.vy - c.vy, c.py - d.py, resMat[2]},
+	// 	{d.vx - e.vx, e.vy - d.vy, d.py - e.py, resMat[3]},
+	// })
+
+	X := DX / D
+	Y := DY / D
+
+	VX := DVX / D
+	// YV := DVY / D
+
+	tempA := (a.px - X) / (VX - a.vx)
+	tempB := (b.px - X) / (VX - b.vx)
+
+	detD := determinant2x2([2][2]float64{
+		{1, tempA},
+		{1, tempB},
+	})
+
+	detDZ := determinant2x2([2][2]float64{
+		{a.pz + tempA*a.vz, tempA},
+		{b.pz + tempB*b.vz, tempB},
+	})
+
+	Z := detDZ / detD
+
+	return X, Y, Z
+}
+
+func Solution2() {
+	allPoints := parseFile("sample.txt")
+
+	X, Y, Z := result(allPoints[0], allPoints[1], allPoints[2], allPoints[3], allPoints[4])
+	fmt.Println("Result: ", X+Y+Z)
+}
